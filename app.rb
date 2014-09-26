8ab97e6aaf140dcce4e30717e6a73cda4a4bd3ae
Fernando Sproviero <fernandospr@gmail.com>
2014-09-25 23:37:22 -0300

    Checking environment variable to use production database

--- app.rb
+++ app.rb
@@ -3,7 +3,13 @@ require 'sinatra'
 require 'mongo'
 require 'json'
 
-DB = Mongo::Connection.new.db('fufodb');
+if ENV['MONGOHQ_URL']
+  uri = URI.parse(ENV['MONGOHQ_URL'])
+  conn = Mongo::Connection.from_uri(ENV['MONGOHQ_URL'])
+  DB = conn.db(uri.path.gsub(/^\//, ''))
+else
+  DB = Mongo::Connection.new.db("fufodb")
+end
 
 get '/entities' do
   content_type :json
